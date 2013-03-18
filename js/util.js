2f0e908b4e2862a2dcc458744f3ca0e647415aec
Claudio Campos <claudio@claudiocampos.com>
2013-03-17 22:21:50 -0400

    refine iterator function

--- js/util.js
+++ js/util.js
@@ -15,21 +15,33 @@ define(function() {
 	};
 
 	var render = function() {
-		$.each( buttons, function( index, value ) {
+/*		$.each( buttons, function( index, value ) {
 
-			var btn = {
-				name: value.name,
-				descendants: value.descendants
-			};
+			var name = value.name;
 
 			$('#container').append( '<div>' );
 
 			$('div')
 				.last()
-				.attr( 'ref', btn.name )
-				.text( btn.name )
-				.data( btn.name, btn );
-		});
+				.attr( 'ref', name )
+				.text( name )
+				.data( value );
+
+			if (!$.isEmptyObject( value.descendants )) {
+				console.log( value.name, ': ', value.descendants );
+			};
+		});*/
+		iterateChildren( buttons );
+
+		function iterateChildren( buttons ) {
+			$.each( buttons, function( index, value ) {
+				console.log( value.name );
+				if (!$.isEmptyObject( value.descendants ) ) {
+					console.log( value.name, ': ', value.descendants );
+					iterateChildren( value.descendants );
+				};
+			});
+		};
 	};
 
 	return {
